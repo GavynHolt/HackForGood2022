2818f97b5aac1891e813d6d3189b15e240e11bd6
Gavyn Holt <gavynholt@gmail.com>
2022-03-25 16:56:41 -0400

    feat: set up basic router

---
+++ src/welcome/Welcome.tsx
@@ -0,0 +1,10 @@
+function Welcome() { 
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl bold">Welcome to Mental Busters, Inc.</h1>
+      <p>We're here to connect you to mental health service and related financial support.</p>
+    </div>
+  );
+}
+
+export default Welcome;
