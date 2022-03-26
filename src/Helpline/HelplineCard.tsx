e4e04ee0b948e7f4984e9564fb711123004746db
Priyank Patel <priyank@rspndr.io>
2022-03-26 15:53:24 -0400

    chore: fix

--- src/Helpline/HelplineCard.tsx
+++ src/Helpline/HelplineCard.tsx
@@ -1,8 +1,4 @@
-import { useNavigate } from "react-router-dom";
-
 export default function CategoryCard({ item }: any) {
-    const navigate = useNavigate();
-
     return (
         <div className="rounded overflow-hidden shadow-lg bg-white">
             {/* <img className="w-full" src="https://images.unsplash.com/photo-1579600161224-cac5a2971069?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80" alt="Mountain" /> */}
