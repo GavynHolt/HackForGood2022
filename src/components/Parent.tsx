e8cd8d96e1613d15cca076be8bf00aaed6c879da
Priyank Patel <priyank@rspndr.io>
2022-03-26 08:21:45 -0400

    chore: add results

--- src/components/Parent.tsx
+++ src/components/Parent.tsx
@@ -4,6 +4,7 @@ import Services from "../Services/Services";
 import Welcome from "../welcome/Welcome";
 import Feed from "../Feed/Feed";
 import Tabs from "./Tabs";
+import Results from "../results/Results";
 
 export default function Parent() {
     const location = useLocation();
@@ -16,6 +17,7 @@ export default function Parent() {
                 {path === "/home" && <Feed />}
                 {path === "/discover" && <Welcome />}
                 {path === "/service/:slug" && <Welcome />}
+                {path === "/results" && <Results />}
                 {!slug && path === "/categories" && <Categories />}
                 {!!slug && path.includes('/categories') && <Services />}
             </div>
